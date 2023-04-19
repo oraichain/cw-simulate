0f259b1249c57c20792bf682d147179fb1906431
Pham Tu <tubackkhoa@gmail.com>
2023-04-19 14:23:55 +0700

    add generic error

--- src/modules/wasm/contract.ts
+++ src/modules/wasm/contract.ts
@@ -290,6 +290,12 @@ export class VmError extends ErrImpl<string> {
   }
 }
 
+export class GenericError extends ErrImpl<string> {
+  constructor(msg: string) {
+    super(`Generic error: ${msg}`);
+  }
+}
+
 export class ContractNotFoundError extends VmError {
   constructor(contractAddress: string) {
     super(`contract ${contractAddress} not found`);
