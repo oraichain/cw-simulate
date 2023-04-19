8691d1406d989831c57e4555d8559ae4cf3c27b4
Pham Tu <tubackkhoa@gmail.com>
2023-04-19 14:28:11 +0700

    add generic error

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './CWSimulateApp';
 export * from './SimulateCosmWasmClient';
 export * from './types';
 export * from './store';
+export * from './modules/wasm/error';
 
 import { save, load } from './persist';
 export const persist = { save, load };
