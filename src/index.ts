c0433c47aea7ec75e4d0467e47eedd2ffd9197fc
Pham Tu <tubackkhoa@gmail.com>
2023-08-03 15:35:33 +0700

    export serde as well

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,4 @@ export * from './SimulateCosmWasmClient';
 export * from './types';
 export * from './store';
 export * from './modules/wasm/error';
-
-import { save, load } from './persist';
-export const persist = { save, load };
+export * from './persist';
