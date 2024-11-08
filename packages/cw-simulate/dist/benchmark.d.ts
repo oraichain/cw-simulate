3581671900c0d19ab32affb1bd716bff11450fab
Pham Tu <tubackkhoa@gmail.com>
2024-11-08 15:49:42 +0700

    remove dist

--- packages/cw-simulate/dist/benchmark.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function downloadState(contract: string, downloadPath: string): Promise<void>;
-export declare function downloadStateOld(contract: string, downloadPath: string): Promise<void>;
-//# sourceMappingURL=benchmark.d.ts.map
--- packages/cw-simulate/dist/store/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './transactional';
-//# sourceMappingURL=index.d.ts.map
